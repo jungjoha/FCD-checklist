cdab3db2e3171ca516849310119c01b87656b3b3
jule241 <jule.beckers@gmx.de>
2025-10-07 11:44:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ const ITEMS = [
     label: "Besteht eine Diskrepanz zwischen dem Ausmaß der berichteten Symptome und der Alltagsfunktion?",
     onlyFull: false,
     instruction:
-  "Ein Nachweis interner Inkonsistenz liegt vor, wenn subjektiv berichtete erhebliche kognitive Schwierigkeiten und/oder niedrige neuropsychologische Testergebnisse in starkem Gegensatz stehen zu Beispielen wie:\n\n" +
-  "- der Fähigkeit, einen kognitiv anspruchsvollen Job ohne Schwierigkeiten auszuüben\n" +
+  "Ein Nachweis interner Inkonsistenz liegt vor, wenn subjektiv berichtete erhebliche kognitive Schwierigkeiten und/oder niedrige neuropsychologische Testergebnisse in starkem Gegensatz stehen zu Beispielen wie:\n- der Fähigkeit, einen kognitiv anspruchsvollen Job ohne Schwierigkeiten auszuüben\n" +
   "- im Gespräch beobachtbaren adäquaten sprachlich-kommunikativen Fähigkeiten\n" +
   "- der Fähigkeit, bestimmte Aktivitäten wie das Lesen eines Buches, das Verwalten von Finanzen und Autofahren ohne Schwierigkeiten auszuführen."
   },
